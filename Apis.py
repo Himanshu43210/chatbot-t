aba39fc3b1814a1ce52e3a1cf2ae79619aecb72f
Himanshu Shukla <himanshu@techhelps.co.in>
2023-11-04 11:43:25 +0530

    Apis upadte

--- Apis.py
+++ Apis.py
@@ -1,28 +1,33 @@
 from flask import Flask, jsonify, request
-import sys
-from flask_cors import CORS  # Import CORS
+from flask_cors import CORS
+import os
+from dotenv import load_dotenv
 
-# Assuming the function chat_with_user is in a file named chat.py
+# Load environment variables from .env file
+load_dotenv()
+
+# Assuming the function chat_with_user is in a file named chatbot.py
 sys.path.append("./products/BuilderfloorChatbot")
 from chatbot import text_to_text_conversation
 
 app = Flask(__name__)
 CORS(app)  # Enable CORS for all routes
 
-# If you need to set specific CORS options, you can do so:
-# cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
+# Get the OpenAI key from the .env file
+openai_key = os.getenv("OPENAI_API_KEY")
+if not openai_key:
+    raise ValueError("No OpenAI API key set in .env file")
+
 
 @app.route("/chat", methods=["POST"])
 def chat_endpoint():
-    data = request.get_json(
-        force=True
-    )  # Ensure we get JSON even if the content-type header is not set.
+    data = request.get_json(force=True)
 
     userQuestion = data.get("userQuestion", "")
     history = data.get("history", "")
-    openai_key = data.get("openai_key", "")  # Extract OpenAI key from request JSON
-    print("Form Apis: ", openai_key)
-    # Pass the OpenAI key to the chatbot
+
+    # No longer necessary to extract OpenAI key from request JSON
+    # Use the key loaded from the .env file instead
     response = text_to_text_conversation(
         userQuestion,
         history,
