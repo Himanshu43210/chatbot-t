197cb3db1cc6d5269fd45c569f22cf3079e19ece
Himanshu Shukla <himanshu@techhelps.co.in>
2023-11-06 12:47:41 +0530

    Changes

--- products/BuilderfloorChatbot/chatbot.py
+++ products/BuilderfloorChatbot/chatbot.py
@@ -1,52 +1,80 @@
+import json
 import os
 from dotenv import load_dotenv
 import openai
 
+# Load the environment variables from .env file
+load_dotenv()
+
 
 def text_to_text_conversation(userQuestion, history):
-    os.environ["OPENAI_API_KEY"] = openai_key
+    # Retrieve the OpenAI API key from environment variables
+    openai_api_key = os.getenv("OPENAI_API_KEY")
+    if openai_api_key is None:
+        raise ValueError("The OPENAI_API_KEY is not set in the environment.")
+
     if userQuestion.lower() == "exit":
         return "Thank You"
 
-    # Setup initial parameters
+    # Setup initial parameters for chat
     model = "gpt-3.5-turbo"
-    prompt = f""" Create a filter in this format: 
-    { 
-        "city": "City which is being searched from the query by default it is Gurgaon" ,
-        "budget": "Array which contain minimum and maximum budget in number. minimum should be 0 unles and untill query does't specify the minimum ammount",
-        "floor": "Array Contain one or more than one Floor options for the Property example 1ST FLOOR ,2ND FLOOR etc",
-        "location": "Array Contain one or more than one locations options for the property example Sushant Lok 1, Sushant Lok 2" ,
-        "size": "Array contain size of the builder floor minumum and maximum size in number",
-        "accommodation": "Array Contain one or more than one accommodation options for the property example 2 BHK, 3 BHK",
-        "possession": "Array Contain one or more than one possession options for the property example Ready, 12M (where M denote months)",
-        "facing": "Array Contain one or more than one facing options for the property example East, West",
-        "sortBy": "When user has specified any sorting of the propertys available, by default it is Price High to Low",
-        "parkFacing": "If the property is park facing or not, either YES or NO",
-        "corner": "If the property is corner facing or not, either YES or NO"
-    }
-    if any filter is not applicable, remove it from json. show only the output json.User Query History: {history} and User Query: {userQuestion}"""
 
-    max_tokens = 150  # Adjust as necessary
+    # Format the prompt as a conversation, if necessary
+    conversation = [
+        {
+            "role": "system",
+            "content": """Create a filter in this format if user is asking general question and filter is not required, thendo not create filter just answer accordingly: 
+            {{ 
+                "city": "City which is being searched from the query by default it is Gurgaon",
+                "budget": "Array which contain minimum and maximum budget in number. minimum should be 0 unless and until query doesn't specify the minimum amount",
+                "floor": "Array Contain one or more than one Floor options for the Property example 1ST FLOOR, 2ND FLOOR etc",
+                "location": "Array Contain one or more than one locations options for the property example Sushant Lok 1, Sushant Lok 2",
+                "size": "Array contain size of the builder floor minimum and maximum size in number",
+                "accommodation": "Array Contain one or more than one accommodation options for the property example 2 BHK, 3 BHK",
+                "possession": "Array Contain one or more than one possession options for the property example Ready, 12M (where M denote months)",
+                "facing": "Array Contain one or more than one facing options for the property example East, West",
+                "sortBy": "When user has specified any sorting of the properties available, by default it is Price High to Low",
+                "parkFacing": "If the property is park facing or not, either YES or NO",
+                "corner": "If the property is corner facing or not, either YES or NO"
+            }}
+            If any filter is not applicable, remove it from json. Show only the output json.""",
+        },
+        {"role": "user", "content": history},
+        {"role": "user", "content": userQuestion},
+    ]
 
-    # Call to OpenAI API
-    response = openai.Completion.create(
+    # Call to OpenAI chat API
+    response = openai.ChatCompletion.create(
         model=model,
-        prompt=prompt,
-        temperature=0.5,  # A neutral temperature, adjust as needed
-        max_tokens=max_tokens,
+        messages=conversation,
+        api_key=openai_api_key,  # Use the API key from the environment variable
     )
 
     # Get the answer from OpenAI
-    answer = response.get("choices", [{}])[0].get("text", "No answer found.").strip()
+    answer = (
+        response.get("choices", [{}])[0]
+        .get("message", {})
+        .get("content", "No answer found.")
+        .strip()
+    )
 
-    return answer
+    # Convert the string answer to a JSON object if it is JSON-formatted
+    try:
+        # Remove any unwanted newlines and backslashes from the string
+        answer = answer.replace("\n", "").replace("\\", "")
+
+        # Attempt to convert the string to a JSON object
+        answer_json = json.loads(answer)
+    except json.JSONDecodeError:
+        # If an error occurs, the answer is not JSON-formatted, so return as is
+        answer_json = answer
+
+    return answer_json
 
 
 # Test the method
 if __name__ == "__main__":
     chatResponse = text_to_text_conversation(
-        "What do you sell and what is my name?",
-        "Hi, My name is Isha. ",
-        "your-openai-api-key",  # Replace with your actual OpenAI API key
+        "What do you sell and what is my name?", "Hi, My name is Isha."
     )
     print(chatResponse)
