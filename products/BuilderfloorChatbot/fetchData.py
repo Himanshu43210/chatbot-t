035b4b4b036bb278070d99fec2b725fcc5f88617
Aashikhatri1 <134989740+Aashikhatri1@users.noreply.github.com>
2023-11-27 21:45:54 +0530

    Create fetchData.py

---
+++ products/BuilderfloorChatbot/fetchData.py
@@ -0,0 +1,59 @@
+from pymongo import MongoClient
+import json
+
+def fetchDataFromMongoDB(filter_data):
+    print("Fetching data from MongoDB with filter:", filter_data)
+    client = MongoClient('mongodb://localhost:27017/') # Replace with your MongoDB URI
+    db = client['real_estate'] # Replace with your database name
+    collection = db['properties'] # Replace with your collection name
+
+    # Base query
+    query = {}
+
+    # Filtering conditions based on filter_data
+    if 'city' in filter_data and filter_data['city']:
+        query['city'] = filter_data['city']
+
+    if 'accommodation' in filter_data and filter_data['accommodation']:
+        query['accommodation'] = {'$in': filter_data['accommodation']}
+
+    if 'location' in filter_data and filter_data['location']:
+        query['sectorNumber'] = {'$in': [location.upper() for location in filter_data['location']]}
+
+    print("MongoDB Query:", query)
+
+    try:
+        cursor = collection.find(query)
+        results = list(cursor)
+
+        property_links = []
+
+        for item in results:
+            if "title" in item and "_id" in item:
+                id = str(item['_id'])
+
+                formatted_string = (
+                    f"https://builderfloor.com/"
+                    f"{item['title'].replace(' ', '-').lower()}-"
+                    f"{item.get('state', '').replace(' ', '_').upper()}-"
+                    f"{item.get('size', '')}SQYD-"
+                    f"{item.get('floor', '').replace(' ', '_').upper()}-"
+                    f"{item.get('accommodation', '').replace(' ', '_')}-"
+                    f"{item.get('facing', '').replace(' ', '_').upper()}-"
+                    f"{item.get('possession', '').replace(' ', '_').upper()}-"
+                    f"{id}"
+                )
+                property_links.append(formatted_string)
+        return {"propertyLinks": property_links}
+
+    except Exception as e:
+        print("Failed to fetch data from MongoDB:", e)
+        return {"propertyLinks": []}
+    finally:
+        client.close()
+
+# Example usage
+filter_data = {'city': 'GURGAON', 'budget': [0, 20000000], 'accommodation': ['3 BHK'], 'location': ['SUSHANT LOK 1']}
+response = fetchDataFromMongoDB(filter_data)
+print("__________________________________________")
+print("response: ", response)
